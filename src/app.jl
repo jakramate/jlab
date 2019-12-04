98b768233b6cd5bb263fe3870047c4c1d3695c21
jkm <jakramate.b@cmu.ac.th>
2019-12-04 15:46:56 +0700

    fix index function

--- src/app.jl
+++ src/app.jl
@@ -1,19 +1,3 @@
-using Mux
-
-@app test = (
-  Mux.defaults,
-  page(respond(index())),
-  page("/about",
-       probabilty(0.1, respond("<h1>Boo!</h1>")),
-       respond("<h1>About Me</h1>")),
-  page("/user/:user", req -> "<h1>Hello, $(req[:params][:user])!</h1>"),
-  Mux.notfound())
-
-fetch(serve(test,parse(Int,ARGS[1])))
-
-
-
-
 function index()
     return "<html>
     <head>
@@ -35,3 +19,22 @@ function index()
 
     </body>
 </html>"
+end
+
+
+using Mux
+
+@app test = (
+  Mux.defaults,
+  page(respond(index())),
+  page("/about",
+       probabilty(0.1, respond("<h1>Boo!</h1>")),
+       respond("<h1>About Me</h1>")),
+  page("/user/:user", req -> "<h1>Hello, $(req[:params][:user])!</h1>"),
+  Mux.notfound())
+
+fetch(serve(test,parse(Int,ARGS[1])))
+
+
+
+
